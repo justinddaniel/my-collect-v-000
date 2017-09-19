5fb2a7d91f71972ee952050b1757f1fcf8af3130
Justin Daniel <justinddaniel@gmail.com>
2017-09-19 15:00:21 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -3,11 +3,10 @@ def my_collect(collection)
   new_collection = []
   if block_given?
     while i < collection.length
-      new_collection << yield (collection[i])
+      new_collection << yield(collection[i])
       i += 1
     end
-  else 
+  else
     "Hey no block was given!"
   end
 end
-
